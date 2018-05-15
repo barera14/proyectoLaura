25e97c826dd35138cde5c56109afa80c59167135
Alejandro Bernal <alejandro45bernal@gmail.com>
2018-05-14 22:36:32 -0500

    proyecto

---
+++ Login/Login/Models/login.aspx
@@ -0,0 +1,54 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="login.aspx.vb" Inherits="Login.login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        #form1 {
+            height: 402px;
+            width: 839px;
+            margin-left: 236px;
+            margin-top: 2px; 
+        }
+    </style>
+     <script language="JavaScript" type="text/javascript">
+             function validar()
+             {
+                 var valida = true;
+ 
+                 if (document.getElementById("TextBox1").value == "" || document.getElementById("Password1").value == "")
+                 {
+                     alert("Introduzca todos los datos");
+                     valida = false;
+                 }
+ 
+                 return valida;
+             }
+        </script>
+</head>
+<body style="width: 1152px; height: 427px">
+    <div>
+    <form id="form1" runat="server" method="post">
+        <asp:Panel ID="Panel1" runat="server" BackColor="#3C7FC5" Height="356px" style="margin-left: 39px" Width="760px">
+            <br />
+            <asp:Label ID="Label1" runat="server" Text="Iniciar Sesión" style="margin-left:40%;font-size: 30px"></asp:Label>
+            <br />
+            <asp:Label ID="Label2" runat="server" Text="Nombre Usuario:" style="margin-left:28%;margin-top:7%"></asp:Label><asp:TextBox ID="TextBox1" runat="server" style="margin-left:10%;margin-top:7%;height:30px;width:200px"></asp:TextBox>
+            <asp:Image ID="Image2" runat="server" Height="52px" ImageUrl="~/Models/registro.png" Width="66px" />
+            <br />
+            <asp:Label ID="Label3" runat="server" Text="Contraseña:" style="margin-left:30%;margin-top:10%"/><input id="Password1" type="password" style="margin-left:13%;margin-top:2%;height:30px;width:200px"/>
+            <asp:Image ID="Image1" runat="server" Height="52px" ImageUrl="~/Models/domain-password.png" style="margin-top: 0px; margin-bottom: 6px" Width="55px" />
+            <br />
+            <br />
+            <asp:Label ID="Label4" runat="server" Text="¿Olvidaste la contraseña?" style="margin-left:30%;margin-top:40%"></asp:Label>
+            <br />
+            <input id="Button1" type="button" value="Crear Usuario" style="margin-left:30%;margin-top:7%" /><input id="Button2" type="button" value="Ingresar" OnClick="validar()" style="margin-left:30%;margin-top:7%"/>
+            
+        </asp:Panel>
+        <br />
+    </form>
+        </div>
+</body>
+</html>
---
+++ Login/Login/Models/login.aspx.designer.vb
@@ -0,0 +1,96 @@
+﻿'------------------------------------------------------------------------------
+' <generado automáticamente>
+'     Este código fue generado por una herramienta.
+'
+'     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+'     se vuelve a generar el código. 
+' </generado automáticamente>
+'------------------------------------------------------------------------------
+
+Option Strict On
+Option Explicit On
+
+
+Partial Public Class login
+    
+    '''<summary>
+    '''Control form1.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents form1 As Global.System.Web.UI.HtmlControls.HtmlForm
+    
+    '''<summary>
+    '''Control Panel1.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents Panel1 As Global.System.Web.UI.WebControls.Panel
+    
+    '''<summary>
+    '''Control Label1.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents Label1 As Global.System.Web.UI.WebControls.Label
+    
+    '''<summary>
+    '''Control Label2.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents Label2 As Global.System.Web.UI.WebControls.Label
+    
+    '''<summary>
+    '''Control TextBox1.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents TextBox1 As Global.System.Web.UI.WebControls.TextBox
+    
+    '''<summary>
+    '''Control Image2.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents Image2 As Global.System.Web.UI.WebControls.Image
+    
+    '''<summary>
+    '''Control Label3.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents Label3 As Global.System.Web.UI.WebControls.Label
+    
+    '''<summary>
+    '''Control Image1.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents Image1 As Global.System.Web.UI.WebControls.Image
+    
+    '''<summary>
+    '''Control Label4.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents Label4 As Global.System.Web.UI.WebControls.Label
+End Class
